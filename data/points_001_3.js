2539ef396ceaed546bc06d44fe6245ed843ef7aa
bulklc <chris.b.logston@gmail.com>
2024-06-24 23:15:37 -0700

    modified:   data/lines_001_2.js 	modified:   data/points_001_3.js 	modified:   index.html 	new file:   index_a.html 	modified:   js/proj4leaflet.js

--- data/points_001_3.js
+++ data/points_001_3.js
@@ -1,30 +1,241 @@
 var json_points_001_3 = {
-"type": "FeatureCollection",
-"name": "points_001_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "desc": "Tesla Water Treatment Facility" }, "geometry": { "type": "Point", "coordinates": [ -121.417765826031797, 37.635796283541211 ] } },
-{ "type": "Feature", "properties": { "desc": "Oakdale Portal" }, "geometry": { "type": "Point", "coordinates": [ -120.599275393239523, 37.802135692056581 ] } },
-{ "type": "Feature", "properties": { "desc": "Roseville Crossover Valves" }, "geometry": { "type": "Point", "coordinates": [ -120.689043466285398, 37.771546376141522 ] } },
-{ "type": "Feature", "properties": { "desc": "San Joaquin River Valve House" }, "geometry": { "type": "Point", "coordinates": [ -121.209604298863113, 37.63782126472767 ] } },
-{ "type": "Feature", "properties": { "desc": "San Antonio Pump Station" }, "geometry": { "type": "Point", "coordinates": [ -121.860888872856336, 37.559098865859902 ] } },
-{ "type": "Feature", "properties": { "desc": "Irvington Portal" }, "geometry": { "type": "Point", "coordinates": [ -121.931799211839433, 37.548890185401888 ] } },
-{ "type": "Feature", "properties": { "desc": "Mocho Shaft" }, "geometry": { "type": "Point", "coordinates": [ -121.662309430351812, 37.601362775307514 ] } },
-{ "type": "Feature", "properties": { "desc": "Sunol Valley Water Treatment Plant" }, "geometry": { "type": "Point", "coordinates": [ -121.856189111523349, 37.535639836017324 ] } },
-{ "type": "Feature", "properties": { "desc": "Sunol Valley Chlorination Facility" }, "geometry": { "type": "Point", "coordinates": [ -121.85799441052886, 37.538572902755362 ] } },
-{ "type": "Feature", "properties": { "desc": "Thomas Shaft Hypochlorite Station" }, "geometry": { "type": "Point", "coordinates": [ -121.494337061133265, 37.613496574408629 ] } },
-{ "type": "Feature", "properties": { "desc": "O'Shaughnesy Dam" }, "geometry": { "type": "Point", "coordinates": [ -119.78814515534053, 37.947895480986865 ] } },
-{ "type": "Feature", "properties": { "desc": "Kirkwood Powerhouse" }, "geometry": { "type": "Point", "coordinates": [ -119.953003915862098, 37.877593902748806 ] } },
-{ "type": "Feature", "properties": { "desc": "Holm Powerhouse" }, "geometry": { "type": "Point", "coordinates": [ -119.968561476019786, 37.896616531493727 ] } },
-{ "type": "Feature", "properties": { "desc": "Moccasin Powerhouse" }, "geometry": { "type": "Point", "coordinates": [ -120.299418574043543, 37.809334660421591 ] } },
-{ "type": "Feature", "properties": { "desc": "Rock River Lime Plant" }, "geometry": { "type": "Point", "coordinates": [ -120.542317410103735, 37.808273671843985 ] } },
-{ "type": "Feature", "properties": { "desc": "Pulgas Dechlorimiation Facility" }, "geometry": { "type": "Point", "coordinates": [ -122.31522645538233, 37.481975777851282 ] } },
-{ "type": "Feature", "properties": { "desc": "Harry Tracy Water Treatment Plant" }, "geometry": { "type": "Point", "coordinates": [ -122.423871934365295, 37.601965495953095 ] } },
-{ "type": "Feature", "properties": { "desc": "University Mound Reservoir" }, "geometry": { "type": "Point", "coordinates": [ -122.411822015572426, 37.724466178372175 ] } },
-{ "type": "Feature", "properties": { "desc": "Lake Merced Pump Station" }, "geometry": { "type": "Point", "coordinates": [ -122.486267084650549, 37.716964270666161 ] } },
-{ "type": "Feature", "properties": { "desc": "Sunset Reservoir" }, "geometry": { "type": "Point", "coordinates": [ -122.483704482161002, 37.75005682911307 ] } },
-{ "type": "Feature", "properties": { "desc": "Merced Manor Reservoir" }, "geometry": { "type": "Point", "coordinates": [ -122.478560600877074, 37.733325615570223 ] } },
-{ "type": "Feature", "properties": { "desc": "Pulgas Water Temple" }, "geometry": { "type": "Point", "coordinates": [ -122.31630208466396, 37.483548677057414 ] } },
-{ "type": "Feature", "properties": { "desc": "Baden Pump Station" }, "geometry": { "type": "Point", "coordinates": [ -122.431983801708114, 37.651813023500814 ] } }
-]
+    "type": "FeatureCollection",
+    "name": "points_001_3",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [{
+            "type": "Feature",
+            "properties": {
+                "desc": "Tesla Water Treatment Facility"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.417765826031797, 37.635796283541211]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Oakdale Portal"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-120.599275393239523, 37.802135692056581]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Roseville Crossover Valves"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-120.689043466285398, 37.771546376141522]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "San Joaquin River Valve House"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.209604298863113, 37.63782126472767]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "San Antonio Pump Station / Sunol Treatment Plant"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.860888872856336, 37.559098865859902]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Irvington Portal"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.931799211839433, 37.548890185401888]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Mocho Shaft"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.662309430351812, 37.601362775307514]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Thomas Shaft Hypochlorite Station"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.494337061133265, 37.613496574408629]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "O'Shaughnesy Dam"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-119.78814515534053, 37.947895480986865]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Kirkwood Powerhouse"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-119.953003915862098, 37.877593902748806]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Holm Powerhouse"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-119.968561476019786, 37.896616531493727]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Moccasin Powerhouse"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-120.299418574043543, 37.809334660421591]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Rock River Lime Plant"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-120.542317410103735, 37.808273671843985]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Pulgas Dechlorimiation Facility"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.31522645538233, 37.481975777851282]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Harry Tracy Water Treatment Plant"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.423871934365295, 37.601965495953095]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "University Mound Reservoir"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.411822015572426, 37.724466178372175]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Lake Merced Pump Station"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.486267084650549, 37.716964270666161]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Sunset Reservoir"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.483704482161002, 37.75005682911307]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Merced Manor Reservoir"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.478560600877074, 37.733325615570223]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Pulgas Water Temple"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.31630208466396, 37.483548677057414]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Baden Pump Station"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.431983801708114, 37.651813023500814]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Alameda East Portal"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.859038110142421, 37.559309225757879]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "desc": "Alameda West Portal"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.86864353921068, 37.557952837104942]
+            }
+        }
+    ]
 }
